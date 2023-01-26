0a0838ffca7db7d9b8bb8072b2b48a9bc7c20734
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2023-01-26 10:51:46 +0300

    Add commit

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,27 +1,30 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
 public class SeleniumTest {
 
-    private WebDriver driver;
+    WebDriver driver;
 
     @BeforeAll
     static void setupAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
 
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setup() {
         ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-        driver = new ChromeDriver();
+
+
     }
 
     @AfterEach
@@ -31,7 +34,7 @@ public class SeleniumTest {
     }
 
     @Test
-    void shouldTest() throws InterruptedException {
+    void shouldTest()  {
         driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79057894561");
@@ -43,17 +46,6 @@ public class SeleniumTest {
 
     }
 
-    @Test
-    void shouldTestWithBug() throws InterruptedException {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ivanov Ivan");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79057894561");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.cssSelector("button")).click();
-        String expected = "Фамилия и имя\n" +
-                "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        String actual = driver.findElement(By.className("input_invalid")).getText().trim();
-        Assertions.assertEquals(expected, actual);
 
-    }
+
 }
